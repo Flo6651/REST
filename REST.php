260e6d712823b4518e7d802176733b326949f009
Florian Gapp <floriangapp12@gmail.com>
2018-03-16 07:14:18 -0700

    reformatting for use in Word

--- REST.php
+++ REST.php
@@ -1,18 +1,19 @@
 <?php
 
 class REST{
-   public $verbose=false;   //if true debugginginformation is printed
-   private $method;     //Contains the http Method
+   public $verbose=false;   // if true
+                           // debugginginformation is printed
+   private $method;     // Contains the http method
    private $request;    // contains the path of REST 
-                        //  (http://domain.at/api.php/$request) 
-                        //  as an array
+                        //(http://domain.at/api.php/$request) 
+                        // as an array
    private $input;        // contains the decoded body 
-                          //  input send with the request
+                          // input send with the request
    private $registers;    // contains function to execute 
    public $output;        // stores the output as array
    public $success=true;  // remains true until an error occures
    private $data;         // contains data used in every 
-                          //  functions friom $registrs
+                          // functions from $registers
    /*
     * 
     * 
@@ -21,22 +22,26 @@ class REST{
     * */
     
    function __construct($data){
-	  if(@$_SERVER['CONTENT']==null) $_SERVER['CONTENT']=file_get_contents('php://input');
+      if(@$_SERVER['CONTENT']==null)
+         $_SERVER['CONTENT']=file_get_contents('php://input');
       $this->method=$_SERVER['REQUEST_METHOD'];
       $request=@$_SERVER['PATH_INFO'];
       $this->data=$data;
       $this->request=explode('/', trim($request,'/'));   
-      switch($this->request[0]){      //selceting the language
+      switch($this->request[0]){     //selceting the language
          case "XML":
-         $this->output = array("Error"=>"unsopported language");
-         $this->success=false;
-      break;
-      case "JSON":
-         $this->input = json_decode(@$_SERVER['CONTENT'],true);
-      break;
-      default:
-         $this->output = Array("Error"=>"wrong language");
-         $this->success=false;
+            $this->output = 
+               array("Error"=>"unsupported language");
+            $this->success=false;
+         break;
+         case "JSON":
+            $this->input = 
+               json_decode(@$_SERVER['CONTENT'],true);
+         break;
+         default:
+            $this->output = 
+               Array("Error"=>"wrong language");
+            $this->success=false;
       break;
       }
    }
@@ -64,7 +69,7 @@ class REST{
     *    contains the key from the input
     * 
     * @param $default
-    *    contains teh return value if no key is found
+    *    contains the return value if no key is found
     * 
     * @param $type
     *    contains the datatype returned (int|string|bool)
@@ -75,9 +80,12 @@ class REST{
       if(is_array($this->input)){
          if(array_key_exists($key,$this->input)){
             switch($type){
-               case "int": return (int)$this->input[$key];
-               case "string": return (string)$this->input[$key];            
-               case "bool": return (bool)$this->input[$key];
+               case "int": 
+                  return (int)$this->input[$key];
+               case "string": 
+                  return (string)$this->input[$key];            
+               case "bool": 
+                  return (bool)$this->input[$key];
             }
          }
       }
@@ -108,18 +116,19 @@ class REST{
             $this->output=$this->registers[$k]
                ($this,$this->input,$data);
          }else{
-            $this->output = Array("error"=>"Unknown path");
+            $this->output = Array("Error"=>"unknown path");
          }
       }   
-      switch($this->request[0]){      //selceting the language
+      switch($this->request[0]){    //selceting the language
          case "XML":
-         $this->output = array("Error"=>"unsopported language");
-         $this->success=false;
-      break;
-      case "JSON":
-			echo json_encode($this->output);  
+            $this->output = 
+               array("Error"=>"unsupported language");
+            $this->success=false;
          break;
-      default:
+         case "JSON":
+            echo json_encode($this->output);  
+            break;
+         default:
       }
    }
 }
