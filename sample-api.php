3c6b34dc86884291f205d2f3eb9bc4f393c4b343
Florian Gapp <floriangapp12@gmail.com>
2018-03-16 07:15:52 -0700

    added sample-api.php    A sample on how to use the framework

---
+++ sample-api.php
@@ -0,0 +1,33 @@
+<?php
+include("./REST.php");
+$_SERVER['CONTENT']=file_get_contents('php://input');	//
+
+$r=new REST($conn);
+
+/*
+ * called when path is 
+ * e.g. 
+ *    /JSON/path/50
+ *    /JSON/Sensor/testSensor
+ * 
+ * the last element of the path is passed as $data
+ * */
+
+$r->register(function($ref,$input,$data){
+	
+		return Array("Error"=>"Sensor not found");
+	
+},"GET:path/*");
+
+/*
+ * callen when path is 
+ * 
+ * */
+$r->register(function($ref,$input,$data){
+	
+		return Array("Error"=>"Sensor not found");
+	
+},"GET:path/");
+
+
+?>
